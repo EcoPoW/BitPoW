4c7c3154ee7c5a7c45161aa5be8dc5099969a6b8
KJ <jia.kan@deeper.network>
2021-12-14 13:36:11 +0000

    sync and commit

--- store.py
+++ store.py
@@ -47,10 +47,11 @@ def main():
 
     if len(sys.argv) == 3:
         print(sys.argv)
-        if sys.argv[1] in ['key', 'host', 'port']:
+        if sys.argv[1] in ['key', 'host', 'port', 'folder']:
             store_obj[sys.argv[1]] = sys.argv[2]
             with open('./store.json', 'w') as f:
                 f.write(json.dumps(store_obj))
+            return
 
         elif sys.argv[1] == 'add':
             path_to_add = sys.argv[2]
@@ -71,45 +72,42 @@ def main():
             }
             with open('./store.json', 'w') as f:
                 f.write(json.dumps(store_obj))
+            return
 
         elif sys.argv[1] == 'remove':
-            pass
+            return
 
     elif len(sys.argv) == 2:
         if sys.argv[1] == 'status':
             pprint.pprint(store_obj.get('add', {}))
             pprint.pprint(store_obj.get('del', {}))
+            return
 
-        elif sys.argv[1] == 'reset':
-            store_obj['add'] = {}
-            store_obj['del'] = {}
-            with open('./store.json', 'w') as f:
-                f.write(json.dumps(store_obj))
-
-        elif sys.argv[1] == 'commit':
+        elif sys.argv[1] == 'sync':
             key = store_obj['key']
             host = store_obj['host']
             port = store_obj['port']
+            folder = store_obj['folder']
             sender_sk = eth_keys.keys.PrivateKey(open(key, 'rb').read())
             sender = sender_sk.public_key.to_checksum_address()
-
-            chain_blocks = set()
-            chain_proofs = set()
-            subchain_blocks = set()
-            subchain_proofs = set()
+            fullstate_hash = store_obj.get('fullstate_hash')
+            fullstate_dict = store_obj.get('fullstate_dict', {})
 
             rsp = requests.get('http://%s:%s/get_highest_subchain_block_hash?sender=%s' % (host, port, sender))
             highest_subchain_hash = rsp.json()['hash']
-            prev_hash = highest_subchain_hash
+            block_hash = highest_subchain_hash
             print('sender', sender)
+            block_stack = []
             while True:
-                print('  prev_hash', prev_hash)
-                rsp = requests.get('http://%s:%s/get_subchain_block?hash=%s' % (host, port, prev_hash))
+                print('  block_hash', block_hash)
+                rsp = requests.get('http://%s:%s/get_subchain_block?hash=%s' % (host, port, block_hash))
                 subchain_block = rsp.json()['msg']
+                print('    data', subchain_block[5])
                 # print('assert', subchain_block)
                 if subchain_block is None:
                     break
-                prev_hash = subchain_block[1]
+                block_stack.append(block_hash)
+                block_hash = subchain_block[1]
                 assert subchain_block[2] == sender
                 data = subchain_block[6]
                 # subchain_blocks.update(data.get("blocks", []))
@@ -118,6 +116,105 @@ def main():
                 if subchain_block[4] == 1:
                     break
 
+            print('block stack', block_stack)
+            while True:
+                if not block_stack:
+                    break
+                block_hash = block_stack.pop()
+                print(block_hash)
+                rsp = requests.get('http://%s:%s/get_subchain_block?hash=%s' % (host, port, block_hash))
+                subchain_block = rsp.json()['msg']
+                print('    data', subchain_block[5])
+                msg = subchain_block[5]
+                if msg.get('type') == 'folder_storage' and msg.get('name') == folder:
+                    current_folder_state_dict = fullstate_dict.setdefault('folder_storage', {}).get(msg['name'], {})
+                    print('current folder', current_folder_state_dict)
+                    if 'remove' in msg:
+                        remove_dict = msg['remove']
+                        for path, info in remove_dict.items():
+                            print('remove', path, info)
+
+                    if 'add' in msg:
+                        add_dict = msg['add']
+                        for path, info in add_dict.items():
+                            print('add', path, info)
+                    # print('fullstate dict', fullstate_dict)
+            print('fullstate dict', fullstate_dict)
+
+            return
+
+        elif sys.argv[1] == 'reset':
+            store_obj['add'] = {}
+            store_obj['del'] = {}
+            with open('./store.json', 'w') as f:
+                f.write(json.dumps(store_obj))
+            return
+
+        elif sys.argv[1] == 'commit':
+            key = store_obj['key']
+            host = store_obj['host']
+            port = store_obj['port']
+            folder = store_obj['folder']
+            add = store_obj.get('add')
+            remove = store_obj.get('remove')
+            sender_sk = eth_keys.keys.PrivateKey(open(key, 'rb').read())
+            sender = sender_sk.public_key.to_checksum_address()
+
+            chain_blocks = set()
+            chain_proofs = set()
+            subchain_blocks = set()
+            subchain_proofs = set()
+
+            fullstate_hash = store_obj.get('fullstate_hash')
+            fullstate_dict = store_obj.get('fullstate_dict', {})
+
+            rsp = requests.get('http://%s:%s/get_highest_subchain_block_hash?sender=%s' % (host, port, sender))
+            highest_subchain_hash = rsp.json()['hash']
+            block_hash = highest_subchain_hash
+            print('sender', sender)
+            block_stack = []
+            while True:
+                print('  block_hash', block_hash)
+                rsp = requests.get('http://%s:%s/get_subchain_block?hash=%s' % (host, port, block_hash))
+                subchain_block = rsp.json()['msg']
+                print('    block', subchain_block[5])
+                # print('assert', subchain_block)
+                if subchain_block is None:
+                    break
+                block_stack.append(block_hash)
+                block_hash = subchain_block[1]
+                assert subchain_block[2] == sender
+                data = subchain_block[6]
+                # subchain_blocks.update(data.get("blocks", []))
+                # subchain_proofs.update(data.get("proofs", []))
+                # print(subchain_block[4])
+                if subchain_block[4] == 1:
+                    break
+
+            print('block stack', block_stack)
+            while True:
+                if not block_stack:
+                    break
+                block_hash = block_stack.pop()
+                print(block_hash)
+                rsp = requests.get('http://%s:%s/get_subchain_block?hash=%s' % (host, port, block_hash))
+                subchain_block = rsp.json()['msg']
+                print('    block', subchain_block[5])
+                msg = subchain_block[5]
+                if msg.get('type') == 'folder_storage' and msg.get('name') == folder:
+                    current_folder_state_dict = fullstate_dict.setdefault('folder_storage', {}).get(msg['name'], {})
+                    print('current folder', current_folder_state_dict)
+                    if 'remove' in msg:
+                        remove_dict = msg['remove']
+                        for path, info in remove_dict.items():
+                            print('remove', path, info)
+
+                    if 'add' in msg:
+                        add_dict = msg['add']
+                        for path, info in add_dict.items():
+                            print('add', path, info)
+
+                    print('fullstate dict', fullstate_dict)
             # amount = 0
             # proofs = chain_proofs - subchain_proofs
             # for hash in proofs:
@@ -127,11 +224,15 @@ def main():
             #     amount += int(2**256/int(hash, 16))
             # data = {'proofs': list(proofs), 'blocks': list(blocks), "amount": amount}
             data = {
-                'type': 'storage_contract',
+                'type': 'folder_storage',
+                'name': folder,
                 'version': 1
             }
+            if remove:
+                data['remove'] = remove
+            if add:
+                data['add'] = add
             data_json = json.dumps(data)
-            receiver = '0x'
 
             rsp = requests.get('http://%s:%s/get_subchain_block?hash=%s' % (host, port, highest_subchain_hash))
             highest_subchain_block = rsp.json()['msg']
@@ -143,52 +244,58 @@ def main():
                 highest_prev_hash = '0'*64
 
             new_timestamp = time.time()
+            # receiver = '0x'
+            receiver = sender
             block_hash = hashlib.sha256((highest_prev_hash + sender + receiver + str(height+1) + data_json + str(new_timestamp)).encode('utf8')).hexdigest()
             signature = sender_sk.sign_msg(str(block_hash).encode("utf8"))
             # print('signature', signature.to_hex())
             new_subchain_block = [block_hash, highest_prev_hash, sender, receiver, height+1, data, new_timestamp, signature.to_hex()]
             rsp = requests.post('http://%s:%s/new_subchain_block?sender=%s' % (host, port, sender), json = new_subchain_block)
-            new_contract_address = '0x%s' % new_subchain_block[0]
-            print("new contract address", new_contract_address)
-            print("  subchain block", new_subchain_block)
+            # new_contract_address = '0x%s' % new_subchain_block[0]
+            # print("new contract address", new_contract_address)
+            print("new subchain block", new_subchain_block)
 
-            t0 = time.time()
-            for i in range(1):
-                data = {
-                    'action': 'update',
-                    'crypto': 'AES_OFB',
-                    'iv': '0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f',
-                    'remove': {
-                        'aes_encrypted_folder1': {
-                            'aes_encrypted_file_path_and_name_in_hex': {
-                                'chunks': [],
-                                'size': (2**20)*11,
-                                'time': ''
-                            }
-                        }
-                    },
-                    'add': {
-                        'aes_encrypted_folder1': {
-                            'aes_encrypted_file_path_and_name_in_hex': {
-                                'chunks': [],
-                                'size': (2**20)*11,
-                                'time': ''
-                            }
-                        }
-                    }
-                }
-                data_json = json.dumps(data)
-                highest_prev_hash = new_subchain_block[0]
-                height = new_subchain_block[4]
-                new_timestamp = time.time()
-                block_hash = hashlib.sha256((highest_prev_hash + sender + new_contract_address + str(height+1) + data_json + str(new_timestamp)).encode('utf8')).hexdigest()
-                signature = sender_sk.sign_msg(str(block_hash).encode("utf8"))
-                # print('signature', signature.to_hex())
-                new_subchain_block = [block_hash, highest_prev_hash, sender, new_contract_address, height+1, data, new_timestamp, signature.to_hex()]
-                rsp = requests.post('http://%s:%s/new_subchain_block?sender=%s' % (host, port, sender), json = new_subchain_block)
-                # print("new subchain block", new_subchain_block)
-            print(time.time() - t0)
+            # t0 = time.time()
+            # for i in range(1):
+            #     data = {
+            #         'action': 'update',
+            #         'crypto': 'AES_OFB',
+            #         'iv': '0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f',
+            #         'remove': {
+            #             'aes_encrypted_folder1': {
+            #                 'aes_encrypted_file_path_and_name_in_hex': {
+            #                     'chunks': [],
+            #                     'size': (2**20)*11,
+            #                     'time': ''
+            #                 }
+            #             }
+            #         },
+            #         'add': {
+            #             'aes_encrypted_folder1': {
+            #                 'aes_encrypted_file_path_and_name_in_hex': {
+            #                     'chunks': [],
+            #                     'size': (2**20)*11,
+            #                     'time': ''
+            #                 }
+            #             }
+            #         }
+            #     }
+            #     data_json = json.dumps(data)
+            #     highest_prev_hash = new_subchain_block[0]
+            #     height = new_subchain_block[4]
+            #     new_timestamp = time.time()
+            #     block_hash = hashlib.sha256((highest_prev_hash + sender + new_contract_address + str(height+1) + data_json + str(new_timestamp)).encode('utf8')).hexdigest()
+            #     signature = sender_sk.sign_msg(str(block_hash).encode("utf8"))
+            #     # print('signature', signature.to_hex())
+            #     new_subchain_block = [block_hash, highest_prev_hash, sender, new_contract_address, height+1, data, new_timestamp, signature.to_hex()]
+            #     rsp = requests.post('http://%s:%s/new_subchain_block?sender=%s' % (host, port, sender), json = new_subchain_block)
+            #     # print("new subchain block", new_subchain_block)
+            # print(time.time() - t0)
+            return
 
+    elif len(sys.argv) == 1:
+        print('help')
+        return
 
     # rsp = requests.get('http://%s:%s/get_highest_block_hash' % (host, port))
     # print(rsp.json())
@@ -230,25 +337,6 @@ def main():
     #         break
     #     # print('-')
 
-    rsp = requests.get('http://%s:%s/get_highest_subchain_block_hash?sender=%s' % (host, port, sender))
-    highest_subchain_hash = rsp.json()['hash']
-    prev_hash = highest_subchain_hash
-    print('sender', sender)
-    while True:
-        print('  prev_hash', prev_hash)
-        rsp = requests.get('http://%s:%s/get_subchain_block?hash=%s' % (host, port, prev_hash))
-        subchain_block = rsp.json()['msg']
-        # print('assert', subchain_block)
-        if subchain_block is None:
-            break
-        prev_hash = subchain_block[1]
-        assert subchain_block[2] == sender
-        data = subchain_block[6]
-        # subchain_blocks.update(data.get("blocks", []))
-        # subchain_proofs.update(data.get("proofs", []))
-        # print(subchain_block[4])
-        if subchain_block[4] == 1:
-            break
 
     # amount = 0
     # proofs = chain_proofs - subchain_proofs
@@ -279,7 +367,7 @@ def main():
     rsp = requests.post('http://%s:%s/new_subchain_block?sender=%s' % (host, port, sender), json = new_subchain_block)
     new_contract_address = '0x%s' % new_subchain_block[0]
     print("new contract address", new_contract_address)
-    print("  subchain block", new_subchain_block)
+    print("  subchain block", new_subchain_block, '\n')
 
     t0 = time.time()
     for i in range(1):
@@ -289,9 +377,10 @@ def main():
         block_hash = hashlib.sha256((highest_prev_hash + sender + new_contract_address + str(height+1) + data_json + str(new_timestamp)).encode('utf8')).hexdigest()
         signature = sender_sk.sign_msg(str(block_hash).encode("utf8"))
         # print('signature', signature.to_hex())
+        print("call contract address", new_contract_address)
         new_subchain_block = [block_hash, highest_prev_hash, sender, new_contract_address, height+1, data, new_timestamp, signature.to_hex()]
         rsp = requests.post('http://%s:%s/new_subchain_block?sender=%s' % (host, port, sender), json = new_subchain_block)
-        # print("new subchain block", new_subchain_block)
+        print("  subchain block", new_subchain_block)
     print(time.time() - t0)
 
 if __name__ == '__main__':
