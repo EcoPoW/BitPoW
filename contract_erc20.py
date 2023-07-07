7f387a8b7ccf668953d57147ddc052d8b45e1fd1
KJ <kernel1983@gmail.com>
2023-07-07 16:14:11 +0800

    erc20 update

--- contract_erc20.py
+++ contract_erc20.py
@@ -1,10 +1,30 @@
 
-import hashlib
 
 import tornado.escape
-import web3
 
 import database
+
+class address(str):pass
+class uint256(int):pass
+
+class MPT:
+    # def __setitem__(self, key, value):
+    def put(self, key, value):
+        global _mpt
+        _mpt.put(key, value)
+
+
+    # def __getitem__(self, key):
+    def get(self, key, default):
+        global _mpt
+        try:
+            current_json = _mpt.get(b'%s_%s' % (CONTRACT_ADDRESS, key.encode('utf8')))
+            current_value = tornado.escape.json_decode(current_json)
+        except:
+            current_value = default
+
+        return current_value
+
 
 # function name() public view returns (string)
 # function symbol() public view returns (string)
@@ -19,15 +39,9 @@ import database
 # event Transfer(address indexed _from, address indexed _to, uint256 _value)
 # event Approval(address indexed _owner, address indexed _spender, uint256 _value)
 
+CONTRACT_ADDRESS = b'0x0000000000000000000000000000000000000001'
 
 _sender = None
-
-_balance = {
-    '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266': 10**20
-}
-# hardhat test Account #0: 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
-# Private Key: 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
-
 
 # def init(_name, _symbol, _decimals):
 #     global name
@@ -41,81 +55,54 @@ _balance = {
 #     pass
 
 
-def mint(_to, _amount):
-    to_bytes = web3.Web3.to_bytes(hexstr=_to)
-    to_addr = web3.Web3.to_checksum_address(to_bytes[12:])
-    amount = web3.Web3.to_int(hexstr=_amount)
-    print('mint', to_addr, amount)
+def mint(_to:address, _value:uint256):
+    current_amount = mpt.get('balance_%s' % _to, 0)
+    new_amount = current_amount + _value
+    print('before mint', current_amount)
+    print('mint to', _to, _value)
+    print('after mint', new_amount)
+    new_amount_json = tornado.escape.json_encode(new_amount)
+    _mpt.update(b'%s_balance_%s' % (CONTRACT_ADDRESS, _to.encode('utf8')), new_amount_json.encode('utf8'))
 
-    prev_contract_hash = db.get(b'chain_%s' % '0x0000000000000000000000000000000000000001'.encode('utf8'))
-    print('mint', prev_contract_hash)
-    msgstate_bytes = db.get(b'msgstate_%s' % prev_contract_hash)
-    msgstate = tornado.escape.json_decode(msgstate_bytes)
-    print('mint', msgstate)
-    msg = db.get(b'msg_%s' % prev_contract_hash)
-    print('mint', msg)
-
-    new_msg = [prev_contract_hash.decode('utf8'), '', to_addr, amount]
-    print('mint', to_addr, amount)
-    new_contract_state = msgstate
-    new_contract_state['balance'].setdefault(to_addr, 0)
-    new_contract_state['balance'][to_addr] += amount
-    new_contract_hash = hashlib.sha256(tornado.escape.json_encode(new_msg).encode('utf8')).hexdigest()
-    new_contract_hash_bytes = new_contract_hash.encode('utf8')
-    db.put(b'msgstate_%s' % new_contract_hash_bytes, tornado.escape.json_encode(new_contract_state).encode('utf8'))
-    db.put(b'msg_%s' % new_contract_hash_bytes, tornado.escape.json_encode([new_contract_hash] + new_msg).encode('utf8'))
-    db.put(b'chain_%s' % '0x0000000000000000000000000000000000000001'.encode('utf8'), new_contract_hash_bytes)
+    current_total_json = _mpt.get(b'%s_total' % CONTRACT_ADDRESS)
+    current_total = tornado.escape.json_decode(current_total_json)
+    new_total = current_total + _value
+    print('after mint total', new_total)
+    new_total_json = tornado.escape.json_encode(new_total)
+    _mpt.update(b'%s_total' % CONTRACT_ADDRESS, new_total_json.encode('utf8'))
 
 
-def approve():
+def approve(_spender:address, _value:uint256):
     pass
 
 
-def allowance():
+def allowance(_owner:address, _spender:address):
     pass
 
 
-def transfer(_to, _amount):
-    to_bytes = web3.Web3.to_bytes(hexstr=_to)
-    to_addr = web3.Web3.to_checksum_address(to_bytes[12:])
-    amount = web3.Web3.to_int(hexstr=_amount)
+def transfer(_to:address, _value:uint256):
+    print('transfer to', _to, _value)
+    sender_amount = mpt.get('balance_%s' % _sender, 0)
+    sender_new_amount = sender_amount - _value
+    assert sender_new_amount >= 0
+    print('after transfer sender', sender_new_amount)
+    sender_new_amount_json = tornado.escape.json_encode(sender_new_amount)
+    _mpt.update(b'%s_balance_%s' % (CONTRACT_ADDRESS, _sender.encode('utf8')), sender_new_amount_json.encode('utf8'))
 
-    prev_contract_hash = db.get(b'chain_%s' % '0x0000000000000000000000000000000000000001'.encode('utf8'))
-    print('transfer', prev_contract_hash)
-    msgstate_bytes = db.get(b'msgstate_%s' % prev_contract_hash)
-    msgstate = tornado.escape.json_decode(msgstate_bytes)
-    print('transfer', msgstate)
-    msg = db.get(b'msg_%s' % prev_contract_hash)
-    print('transfer', msg)
-
-    new_msg = [prev_contract_hash.decode('utf8'), '', to_addr, amount]
-    print('transfer', _sender, to_addr, amount)
-    new_contract_state = msgstate
-    new_contract_state['balance'].setdefault(_sender, 0)
-    new_contract_state['balance'][_sender] -= amount
-    new_contract_state['balance'].setdefault(to_addr, 0)
-    new_contract_state['balance'][to_addr] += (amount - 10**16)
-    new_contract_hash = hashlib.sha256(tornado.escape.json_encode(new_msg).encode('utf8')).hexdigest()
-    new_contract_hash_bytes = new_contract_hash.encode('utf8')
-    db.put(b'msgstate_%s' % new_contract_hash_bytes, tornado.escape.json_encode(new_contract_state).encode('utf8'))
-    db.put(b'msg_%s' % new_contract_hash_bytes, tornado.escape.json_encode([new_contract_hash] + new_msg).encode('utf8'))
-    db.put(b'chain_%s' % '0x0000000000000000000000000000000000000001'.encode('utf8'), new_contract_hash_bytes)
+    current_amount = mpt.get('balance_%s' % _sender, 0)
+    new_amount = current_amount + _value
+    print('after transfer receiver', new_amount)
+    new_amount_json = tornado.escape.json_encode(new_amount)
+    _mpt.update(b'%s_balance_%s' % (CONTRACT_ADDRESS, _to.encode('utf8')), new_amount_json.encode('utf8'))
 
 
-def transferFrom():
+def transferFrom(_from:address, _to:address, _value:uint256):
     print('transferFrom')
 
 
-def balanceOf(user):
-    user_bytes = web3.Web3.to_bytes(hexstr=user)
-    user_addr = web3.Web3.to_checksum_address(user_bytes[12:])
-    prev_contract_hash = db.get(b'chain_%s' % '0x0000000000000000000000000000000000000001'.encode('utf8'))
-    print('balanceOf', prev_contract_hash)
-    msgstate_bytes = db.get(b'msgstate_%s' % prev_contract_hash)
-    msgstate = tornado.escape.json_decode(msgstate_bytes)
-    print('balanceOf', msgstate)
-    # amount = _balance.get(user_addr, 0)
-    amount = msgstate['balance'].get(user_addr, 0)
+def balanceOf(_owner:address):
+    amount = mpt.get('balance_%s' % _owner, 0)
+    print('balanceOf', _owner, amount)
 
     return f'0x{amount:0>64x}'
     # return '0x0000000000000000000000000000000000000000000000000000000000001000'
@@ -134,43 +121,34 @@ def decimals():
     return f'0x{18:0>64x}'
 
 def totalSupply():
-    return 0
+    amount = mpt.get('total', 0)
+    return f'0x{amount:0>64x}'
 
 
-import eth_utils
+database.get_conn()
+_mpt = database.get_mpt()
+_mpt.update(b'%s_balance_0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266' % CONTRACT_ADDRESS, tornado.escape.json_encode(10**20))
+_mpt.update(b'%s_total' % CONTRACT_ADDRESS, tornado.escape.json_encode(10**20))
+print('root', _mpt.root())
+print('root hash', _mpt.root_hash())
 
-interface_map = {
-    '0x'+eth_utils.keccak(b'transfer(address,uint256)').hex()[:8]: transfer,
-    '0x'+eth_utils.keccak(b'balanceOf(address)').hex()[:8]: balanceOf,
-    '0x'+eth_utils.keccak(b'decimals()').hex()[:8]: decimals,
-    '0x'+eth_utils.keccak(b'allowance(address,address)').hex()[:8]: allowance,
-    '0x'+eth_utils.keccak(b'symbol()').hex()[:8]: symbol,
-    '0x'+eth_utils.keccak(b'totalSupply()').hex()[:8]: totalSupply,
-    '0x'+eth_utils.keccak(b'name()').hex()[:8]: name,
-    '0x'+eth_utils.keccak(b'approve(address,uint256)').hex()[:8]: approve,
-    '0x'+eth_utils.keccak(b'transferFrom(address,address,uint256)').hex()[:8]: transferFrom,
-    '0x'+eth_utils.keccak(b'mint(address,uint256)').hex()[:8]: mint,
-}
+mpt = MPT()
 
-# transfer(address,uint256)： 0xa9059cbb
-# balanceOf(address)：0x70a08231
-# decimals()：0x313ce567
-# allowance(address,address)： 0xdd62ed3e
-# symbol()：0x95d89b41
-# totalSupply()：0x18160ddd
-# name()：0x06fdde03
-# approve(address,uint256)：0x095ea7b3
-# transferFrom(address,address,uint256)： 0x23b872dd
+# hardhat test Account #0: 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
+# Private Key: 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
 
-db = database.get_conn()
-blockhash = db.get(b'chain_%s' % '0x0000000000000000000000000000000000000001'.encode('utf8'))
-print(blockhash)
-if not blockhash:
-    contract_state = {'balance': _balance}
-    new_msg = ['0'*64, '', '', contract_state]
-    new_contract_hash = hashlib.sha256(tornado.escape.json_encode(new_msg).encode('utf8')).hexdigest()
-    new_contract_hash_bytes = new_contract_hash.encode('utf8')
 
-    db.put(b'msgstate_%s' % new_contract_hash_bytes, tornado.escape.json_encode(contract_state).encode('utf8'))
-    db.put(b'msg_%s' % new_contract_hash_bytes, tornado.escape.json_encode([new_contract_hash] + new_msg).encode('utf8'))
-    db.put(b'chain_%s' % '0x0000000000000000000000000000000000000001'.encode('utf8'), new_contract_hash_bytes)
+if __name__ == '__main__':
+    for i in range(2):
+        mint('0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266', 1000)
+    _sender = '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266'
+    transfer('0x0000000000000000000000000000000000000002', 1000)
+    balanceOf('0x0000000000000000000000000000000000000002')
+    print(totalSupply())
+    print(symbol())
+    print(decimals())
+
+    # t0 = time.time()
+    # for i in range(10000):
+    #     balanceOf('0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266')
+    # print(time.time() - t0)
