7a90f4ce59f587a1a6af660ea22db2679c4c5aa5
KJ <kernel1983@gmail.com>
2023-07-22 02:37:09 +0800

    mint to a fixed account

--- new_contract_interaction.py
+++ new_contract_interaction.py
@@ -1,4 +1,6 @@
 
+import json
+
 import web3
 import eth_account
 
@@ -356,22 +358,43 @@ contract_abi = '''[
 ]'''
 
 
+try:
+    f = open('test_keyfile.json', 'rt')
+    keyfile_json = f.read()
+    f.close()
+    keyfile_dict = json.loads(keyfile_json)
+    account_key = eth_account.account.decode_keyfile_json(keyfile_dict, password=b'')
+    account = eth_account.Account.from_key(account_key)
+    print('account read', account.address)
+
+except:
+    account = eth_account.Account.create()
+    print('account create', account.address)
+    keyfile_dict = eth_account.account.create_keyfile_json(private_key=account.key, password=b'')
+    f = open('test_keyfile.json', 'wt')
+    keyfile_json = json.dumps(keyfile_dict)
+    f.write(keyfile_json)
+    f.close()
+
+# print('keyfile_json', keyfile_json)
+
 erc20 = w3.eth.contract(address='0x0000000000000000000000000000000000000001', abi=contract_abi)
-balance = erc20.functions.balanceOf('0x0000000000000000000000000000000000000001').call()
-print(balance)
-totalsupply = erc20.functions.totalSupply().call()
-print(totalsupply)
+
 
 # mint = erc20.functions.mint('0x0000000000000000000000000000000000000001', 1000).transact()
 # print(mint)
 
-account = eth_account.Account.create()
 nonce = w3.eth.get_transaction_count(account.address)
-unsigned_tx = erc20.functions.mint('0x0000000000000000000000000000000000000001', 1000).build_transaction({
+unsigned_tx = erc20.functions.mint(account.address, 1000).build_transaction({
     'from': account.address,
     'nonce': nonce,
 })
-print(unsigned_tx)
+# print(unsigned_tx)
 signed_tx = w3.eth.account.sign_transaction(unsigned_tx, private_key=account.key)
 # print(signed_tx)
 tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
+
+balance = erc20.functions.balanceOf(account.address).call()
+print(balance)
+totalsupply = erc20.functions.totalSupply().call()
+print(totalsupply)
