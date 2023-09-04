1633f1f0ef7b8bfe104a7524bbc0361e38a19844
KJ <kernel1983@gmail.com>
2023-09-04 21:36:50 +0800

    update console

--- console.py
+++ console.py
@@ -1,13 +1,16 @@
 
-import traceback
-
+#import traceback
+import inspect
 
 def log(*t):
-    funcs = []
-    for line in traceback.format_stack():
-        func = line.split('\n')[1].strip()
-        funcs.append(func)
-    print('> '+'\n> '.join(funcs[:-2]))
-    print(*t)
     print()
+    #print(inspect.stack()[1])
+    #print(inspect.stack()[1].frame)
+    print(inspect.stack()[1].filename, inspect.stack()[1].lineno, inspect.stack()[1].function)
+    #funcs = []
+    #for line in traceback.format_stack():
+    #    func = line.split('\n')[1].strip()
+    #    funcs.append(func)
+    #print('> '+'\n> '.join(funcs[:-2]))
+    print(*t)
 
