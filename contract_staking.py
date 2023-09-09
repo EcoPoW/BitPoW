0a7e819891e4827df9d0e0bec1d8aa509f47a19c
KJ <kernel1983@gmail.com>
2023-09-09 17:59:20 +0800

    fork staking and contract

---
+++ contract_staking.py
@@ -0,0 +1,117 @@
+
+
+import tornado.escape
+
+from state import address, uint256
+# from state import _state
+# from state import _sender
+
+# function name() public view returns (string)
+# function symbol() public view returns (string)
+# function decimals() public view returns (uint8)
+# function totalSupply() public view returns (uint256)
+# function balanceOf(address _owner) public view returns (uint256 balance)
+# function transfer(address _to, uint256 _value) public returns (bool success)
+# function transferFrom(address _from, address _to, uint256 _value) public returns (bool success)
+# function approve(address _spender, uint256 _value) public returns (bool success)
+# function allowance(address _owner, address _spender) public view returns (uint256 remaining)
+
+# event Transfer(address indexed _from, address indexed _to, uint256 _value)
+# event Approval(address indexed _owner, address indexed _spender, uint256 _value)
+
+
+# def init(_name, _symbol, _decimals):
+#     global name
+#     global symbol
+#     global decimals
+
+#     if not (name, symbol, decimals):
+#         name = _name
+#         symbol = _symbol
+#         decimals = _decimals
+#     pass
+
+
+def mint(_to:address, _value:uint256):
+    current_amount = _state.get('balance', _to, 0)
+    new_amount = current_amount + _value
+    print('before mint', current_amount)
+    print('mint to', _to, _value)
+    print('after mint', new_amount)
+    _state.put('balance', _to, new_amount)
+
+    current_total = _state.get('total', CONTRACT_ADDRESS, 0)
+    new_total = current_total + _value
+    print('after mint total', new_total)
+    _state.put('total', CONTRACT_ADDRESS, new_total)
+
+
+def approve(_spender:address, _value:uint256):
+    pass
+
+
+def allowance(_owner:address, _spender:address):
+    pass
+
+
+def transfer(_to:address, _value:uint256):
+    print('transfer to', _to, _value)
+    sender_amount = _state.get('balance', _sender, 0)
+    sender_new_amount = sender_amount - _value
+    assert sender_new_amount >= 0
+    print('after transfer sender', sender_new_amount)
+    _state.put('balance', _sender, sender_new_amount)
+
+    to_amount = _state.get('balance', _to, 0)
+    to_new_amount = to_amount + _value
+    print('after transfer receiver', to_new_amount)
+    _state.put('balance', _to, to_new_amount)
+
+
+def transferFrom(_from:address, _to:address, _value:uint256):
+    print('transferFrom')
+
+
+def balanceOf(_owner:address):
+    amount = _state.get('balance', _owner, 0)
+    print('balanceOf', _owner, amount)
+
+    return f'0x{amount:0>64x}'
+    # return '0x0000000000000000000000000000000000000000000000000000000000001000'
+
+
+def name():
+    return None
+
+def symbol():
+    sym = hex(ord('U'))[2:]
+    print('sym', sym)
+    return '0x00000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000001%s00000000000000000000000000000000000000' % sym
+    # return '0x00000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000003504f570000000000000000000000000000000000' #POW
+
+def decimals():
+    return f'0x{18:0>64x}'
+
+def totalSupply():
+    amount = _state.get('total', CONTRACT_ADDRESS, 0)
+    return f'0x{amount:0>64x}'
+
+
+# hardhat test Account #0: 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
+# Private Key: 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
+
+
+if __name__ == '__main__':
+    for i in range(2):
+        mint('0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266', 1000)
+    _sender = '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266'
+    transfer('0x0000000000000000000000000000000000000002', 1000)
+    balanceOf('0x0000000000000000000000000000000000000002')
+    print(totalSupply())
+    print(symbol())
+    print(decimals())
+
+    # t0 = time.time()
+    # for i in range(10000):
+    #     balanceOf('0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266')
+    # print(time.time() - t0)
--- staking.py
+++ staking.py
@@ -1,143 +1,309 @@
 
 import sys
-import uuid
-import random
+import os
+import time
 import hashlib
-import json
-import pprint
-import select
 import multiprocessing
-import math
+import json
+import types
+import pprint
+# import threading
+# import curses
 
-import websocket
+# import eth_hash.auto
+import web3
+import eth_account
+import eth_utils
+import requests
+import hexbytes
 
-# staking = {uuid.uuid4().hex: (random.randint(1, 1000), random.randint(10000000, 100000000000000)) for i in range(5, 500000)}
+import tornado.ioloop
+import tornado.gen
+import tornado.websocket
+import rocksdb
 
-
-# total = sum([i[0] for i in staking.values()])
-# print('total', total)
-
-# blockhash = uuid.uuid4().hex
-# print('blockhash', blockhash)
-
-# pointer = (int(blockhash, 16)) % total
-# print('pointer', pointer)
-# for user, (token, secret) in staking.items():
-#     user_pointer = (int(blockhash, 16) + secret) % total
-#     rate = abs(user_pointer - pointer)/token
-#     if rate < 3:
-#         print(user, rate, token)
-
-
-# class Dispatcher:
-#     def __init__(self, app):
-#         self.app = app
-#         self.ping_timeout = 10
-
-#     def read(self, sock, read_callback, check_callback):
-#         global parent_conns
-#         global hex_encoding
-#         nonce = 0
-#         task = 0
-#         while self.app.keep_running:
-#             r, w, e = select.select(
-#                     (self.app.sock.sock, ), (), (), 0.1)
-#             if r:
-#                 if not read_callback():
-#                     break
-#             check_callback()
-
-#             for idx, conn in enumerate(parent_conns):
-#                 if conn.poll():
-#                     msg_json = conn.recv()
-#                     print(idx, msg_json)
-#                     msg = json.loads(msg_json)
-#                     if msg[0] == 'RESULT' and msg[1] == task:
-#                         hex_encoding = ''
-#                         task += 1
-#                         nonce = msg[2] + 1
-
-#             if not hex_encoding:
-#                 hex_encoding = 'ffffff'
-#                 for idx, conn in enumerate(parent_conns):
-#                     # print(hex_encoding)
-#                     conn.send(json.dumps(['ENCODE', task, hex_encoding, nonce+idx, len(parent_conns)]))
+import vm
+import state
+import eth_tx
+import console
+import contract_erc20
 
 
-def main():
-    global parent_conns
-    # print(sys.argv)
-    if len(sys.argv) < 2:
-        print('help')
-        print('  miner.py key')
-        print('  miner.py host')
-        print('  miner.py port')
-        print('  miner.py mine')
-        return
+if not os.path.exists('miners'):
+    os.makedirs('miners')
+db = rocksdb.DB('miners/mining.db', rocksdb.Options(create_if_missing=True))
 
-    miner_obj = {}
+_state = state.State(db)
+contract_erc20._state = _state
+
+contract_map = {
+    '0x0000000000000000000000000000000000000001': contract_erc20
+}
+
+interface_map = {}
+type_map = {}
+for k, v in contract_erc20.__dict__.items():
+    if not k.startswith('_') and type(v) in [types.FunctionType]:
+        # print(k, type(v))
+        # print(v.__code__.co_kwonlyargcount, v.__code__.co_posonlyargcount)
+        # print(v.__code__.co_varnames[:v.__code__.co_argcount])
+        # for i in v.__code__.co_varnames[:v.__code__.co_argcount]:
+        #     print(v.__annotations__[i].__name__)
+        params = [v.__annotations__[i].__name__ for i in v.__code__.co_varnames[:v.__code__.co_argcount]]
+        func_sig = '%s(%s)' % (k, ','.join(params))
+        # print(func_sig, '0x'+eth_utils.keccak(func_sig.encode('utf8')).hex()[:8])
+        interface_map['0x'+eth_utils.keccak(func_sig.encode('utf8')).hex()[:8]] = v
+        type_map[k] = params
+
+console.log(interface_map)
+console.log(type_map)
+
+vm = vm.VM()
+vm.import_module(contract_erc20)
+
+
+def pow(conn):
+    start = 0
     try:
-        with open('./.miner.json', 'r') as f:
-            miner_obj = json.loads(f.read())
-            pprint.pprint(miner_obj)
+        d = 6
+        sleep = True
+        while True:
+            if conn.poll():
+                m = conn.recv()
+                if m[0] == 'START':
+                    console.log('start', m)
+                    block_hash = m[1]
+                    start = m[2]
+                    #difficulty = m[3]
+                    sleep = False
+                elif m[0] == 'STOP':
+                    sleep = True
+
+            if sleep:
+                time.sleep(5)
+                # print('sleep')
+                continue
+
+            # t0 = time.time()
+            nonce = start
+            for nonce in range(start, start+10000000):
+                if nonce % 100000 == 0:
+                    print(nonce)
+                h = hashlib.sha256(block_hash + str(nonce).encode('utf8')).hexdigest()
+                if h.startswith('0'*d):
+                    console.log(h, nonce)
+                    conn.send(['FOUND', block_hash, nonce])
+                    sleep = True
+                    break
+            else:
+                conn.send(['DONE', block_hash, start, start+10000000])
+                sleep = True
 
     except:
-        print('error')
-
-    if sys.argv[1] in ['key', 'host', 'port']:
-        miner_obj[sys.argv[1]] = sys.argv[2]
-        with open('./.miner.json', 'w') as f:
-            f.write(json.dumps(miner_obj))
-        return
-
-    elif sys.argv[1] == 'mine':
-        # process_number = int(sys.argv[2])
-        parent_conns = []
-
-        # for i in range(process_number):
-        #     parent_conn, child_conn = multiprocessing.Pipe()
-        #     parent_conns.append(parent_conn)
-        #     p = multiprocessing.Process(target=mine, args=(child_conn,))
-        #     p.start()
+        pass
 
 
-        host = miner_obj['host']
-        port = miner_obj['port']
-        ws = websocket.WebSocketApp("ws://%s:%s/miner" % (host, port),
-                              on_open=on_open,
-                              on_message=on_message,
-                              on_error=on_error,
-                              on_close=on_close)
-        # dispatcher = Dispatcher(ws)
-        # ws.run_forever(dispatcher=dispatcher)
-        ws.run_forever()
+class MiningClient:
+    def __init__(self, url, timeout):
+        self.url = url
+        self.timeout = timeout
+        self.ioloop = tornado.ioloop.IOLoop.instance()
+        self.ws = None
+        self.current_mining = None
+        self.next_mining = {}
+        self.header_data = None
 
-        # print(parent_conn.recv())
-        # p.join()
+        self.connect()
+        tornado.ioloop.PeriodicCallback(self.keep_alive, 20000).start()
+        tornado.ioloop.PeriodicCallback(self.poll, 100).start()
+        self.ioloop.start()
 
-def on_message(ws, message):
-    # global hex_encoding
-    # global parent_conns
-    print(message)
-    seq = json.loads(message)
-    if seq[0] == 'HIGHEST_BLOCK':
-        highest_block_height = seq[1]
-        highest_block_hash = seq[2]
-        new_difficulty = seq[3]
-        print(math.log(int(new_difficulty), 2), int(new_difficulty))
-        # highest_block = seq[4]
-        ws.send(json.dumps(['GET_BLOCK_STATE', highest_block_hash]))
+    @tornado.gen.coroutine
+    def connect(self):
+        console.log("trying to connect")
+        try:
+            self.ws = yield tornado.websocket.websocket_connect(self.url)
+        except Exception:
+            console.log("connection error")
+        else:
+            console.log("connected")
+            self.run()
 
+    @tornado.gen.coroutine
+    def run(self):
+        while True:
+            msg = yield self.ws.read_message()
+            if msg is None:
+                console.log("connection closed")
+                self.ws = None
+                break
+            else:
+                #console.log(msg)
+                seq = json.loads(msg)
+                if seq[0] == '_NEW_CHAIN_BLOCK':
+                    # next_mining.append(message['name'])
+                    if not self.current_mining:
+                        self.current_mining = self.next_mining
+                        self.next_mining = {}
+                        commitment = hashlib.sha256(json.dumps(self.current_mining).encode('utf8')).digest()
+                        conn.send(['START', commitment, 0])
 
-def on_error(ws, error):
-    print(error)
+                elif seq[0] == 'NEW_SUBCHAIN_BLOCK':
+                    data = seq[5]
+                    count = data[0]
+                    signature = seq[6]
+                    eth_tx_hash = eth_tx.hash_of_eth_tx_list(data)
+                    signature_obj = eth_account.Account._keys.Signature(bytes.fromhex(signature[2:]))
+                    pubkey = signature_obj.recover_public_key_from_msg_hash(eth_tx_hash)
+                    sender = pubkey.to_checksum_address()
+                    console.log('sender', sender, 'count', count)
+                    console.log('data', data)
 
-def on_close(ws, close_status_code, close_msg):
-    print("close")
+                    txs = self.next_mining.setdefault(sender, [])
+                    txs.append(data)
+                    txbody = []
+                    #statebody = {}
+                    #print('txs', txs)
+                    console.log('current_mining', self.current_mining)
+                    if not self.current_mining:
+                        self.current_mining = self.next_mining
+                        self.next_mining = {}
+                        console.log('current_mining', self.current_mining)
 
-def on_open(ws):
-    ws.send(json.dumps(['GET_HIGHEST_BLOCK']))
+                        req = requests.get('http://127.0.0.1:9001/get_chain_latest')
+                        console.log('get_chain_latest', req.text)
+                        obj = req.json()
+                        if obj['height'] == 0:
+                            parent_hash = '0'*64
+                        else:
+                            parent_hash = obj['blockhashes'][0]
+                        block_number = obj['height']
+                        _state.block_number = block_number + 1
 
+                        for addr in self.current_mining:
+                            #print('current_mining', current_mining)
+                            console.log('current_mining[addr]', addr, self.current_mining[addr])
 
-if __name__ == '__main__':
-    main()
+                        req = requests.get('http://127.0.0.1:9001/get_pool_subchains')
+                        pool_subchains = req.json()
+                        #console.log('get_pool_subchains', req.json())
+                        req = requests.get('http://127.0.0.1:9001/get_state_subchains?addrs=%s&height=%s' % (','.join(pool_subchains.keys()), block_number))
+                        console.log('get_state_subchains', req.text)
+                        state_subchains = req.json()
+
+                        for addr in pool_subchains:
+                            #console.log('current_mining', self.current_mining)
+                            console.log('get_pool_subchains addr', addr, pool_subchains[addr])
+                            to_no, to_hash = pool_subchains[addr]
+                            console.log('get_state_subchains addr', state_subchains[addr])
+                            from_no = 0
+                            if state_subchains[addr]:
+                                from_no = state_subchains[addr]['height']
+                            req = requests.get('http://127.0.0.1:9001/get_pool_blocks?addr=%s&from_no=%s&to_no=%s&to_hash=%s' % (addr, from_no, to_no, to_hash))
+                            txblocks = req.json()['blocks']
+                            txblocks.reverse()
+                            last_tx_hash = None
+                            last_tx_height= None
+                            for txblock in txblocks:
+                                pprint.pprint(txblock)
+                                tx_list = txblock[4]
+                                tx_to = tx_list[4]
+                                tx_data = tx_list[6]
+                                tx_signature_hex = txblock[5]
+                                tx_signature_obj = eth_account.Account._keys.Signature(signature_bytes=hexbytes.HexBytes(tx_signature_hex))
+                                vrs = tx_signature_obj.vrs
+
+                                tx_hash = eth_tx.hash_of_eth_tx_list(tx_list)
+                                tx_from = eth_account.Account._recover_hash(tx_hash, vrs=vrs)
+                                #print('tx_from', tx_from)
+                                # contract_erc20._sender = tx_from
+                                vm.global_vars['_sender'] = tx_from
+                                CONTRACT_ADDRESS = '0x0000000000000000000000000000000000000001'
+                                vm.global_vars['CONTRACT_ADDRESS'] = CONTRACT_ADDRESS
+
+                                func_sig = tx_data[:10]
+                                # print(interface_map[func_sig], tx_data)
+                                func_params_data = tx_data[10:]
+                                func_params = [func_params_data[i:i+64] for i in range(0, len(func_params_data)-2, 64)]
+                                #print('func', interface_map[func_sig].__name__, func_params)
+                                type_params = []
+                                for k, v in zip(type_map[interface_map[func_sig].__name__], func_params):
+                                    # print('type', k, v)
+                                    if k == 'address':
+                                        type_params.append(web3.Web3.to_checksum_address(web3.Web3.to_checksum_address('0x'+v[24:])))
+                                    elif k == 'uint256':
+                                        type_params.append(web3.Web3.to_int(hexstr=v))
+
+                                # result = interface_map[func_sig](*func_params)
+                                vm.run(type_params, interface_map[func_sig].__name__)
+                                last_tx_height = tx_list[0]
+                                last_tx_hash = txblock[0]
+                            txbody.append([addr, last_tx_height, last_tx_hash])
+
+                        console.log(txbody)
+                        console.log(_state.pending_state)
+                        self.txbody_json = json.dumps(txbody)
+                        self.statebody_json = json.dumps(_state.pending_state, sort_keys=True)
+                        txbody_hash = hashlib.sha256(self.txbody_json.encode('utf8')).hexdigest()
+                        statebody_hash = hashlib.sha256(self.statebody_json.encode('utf8')).hexdigest()
+                        console.log(txbody_hash)
+                        console.log(statebody_hash)
+                        _state.merge('')
+
+                        self.header_data = {
+                            'txbody_hash': txbody_hash,
+                            'statebody_hash': statebody_hash,
+                            'height': block_number + 1,
+                            'difficulty': 2**254,
+                            'parent': parent_hash,
+                            'address': '0x'+'0'*40,
+                            'timestamp': 0,
+                        }
+                        block_hash = hashlib.sha256(json.dumps(self.header_data, sort_keys=True).encode('utf8')).digest()
+                        console.log(block_hash)
+                        conn.send(['START', block_hash, 0])
+
+                    else:
+                        pass
+
+    def keep_alive(self):
+        if self.ws is None:
+            self.connect()
+        else:
+            self.ws.write_message('["KEEP_ALIVE"]')
+
+    def poll(self):
+        for conn in cs:
+            if conn.poll(0.1):
+                m = conn.recv()
+                if m[0] == 'DONE':
+                    # continue
+                    console.log(m)
+                    block_hash = m[1]
+                    end = m[3]
+                    console.log(block_hash)
+                    conn.send(['START', block_hash, end])
+
+                elif m[0] == 'FOUND':
+                    # submit to chain
+                    console.log(m)
+                    block_hash = m[1].hex()
+                    nonce = m[2]
+                    message = ['NEW_CHAIN_TXBODY', block_hash, self.header_data['height'], self.txbody_json]
+                    self.ws.write_message(json.dumps(message))
+                    message = ['NEW_CHAIN_STATEBODY', block_hash, self.header_data['height'], self.statebody_json]
+                    self.ws.write_message(json.dumps(message))
+                    message = ['NEW_CHAIN_HEADER', block_hash, self.header_data, nonce]
+                    self.ws.write_message(json.dumps(message))
+                    self.current_mining = None
+
+ps = []
+cs = []
+if __name__ == "__main__":
+    conn, child_conn = multiprocessing.Pipe()
+    process = multiprocessing.Process(target=pow, args=(child_conn,))
+    ps.append(process)
+    cs.append(conn)
+    process.start()
+    client = MiningClient("ws://127.0.0.1:9001/miner", 5)
+
